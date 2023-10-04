e858a27e6f0cc907e0db4ea6daf3ceebe59a3292
Nirina VELOSONINA <vnirina081@gmail.com>
2023-10-04 18:41:33 +0200

    ajout de page Error

---
+++ argentbank/src/Pages/Error.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Error = () => {
+  return (
+    <div className='error404'>
+      <h1>404</h1>
+      <p>Oups! La page que vous demandez n'existe pas.</p>
+      <Link to='/'>Retour à la page d'accueil</Link>
+    </div>
+  );
+};
+
+export default Error;
