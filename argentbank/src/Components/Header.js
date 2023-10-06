17d67f3d5a35a449d9dd4260ed1d185f66f0564c
Nirina VELOSONINA <vnirina081@gmail.com>
2023-10-06 15:03:31 +0200

    modification du Header

--- argentbank/src/Components/Header.js
+++ argentbank/src/Components/Header.js
@@ -1,26 +1,29 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom'; 
 import logo from '../assets/images/argentBankLogo.png';
 
 const Header = () => {
   return (
     <nav className="main-nav">
-      <NavLink to="/">
-        <img
-          className="main-nav-logo-image"
-          src={logo}
-          alt="Argent Bank Logo"
-        />
-        <h1 className="sr-only">Argent Bank</h1>
-      </NavLink>
-      <ul>
-        <li>
+      <Link to="/">
+        <div className="main-nav-logo">
+          <img
+            className="main-nav-logo-image"
+            src={logo}
+            alt="Argent Bank Logo"
+          />
+          <h1 className="sr-only">Argent Bank</h1>
+        </div>
+      </Link>
+      <div>
+        <Link className="main-nav-item" to="/sign-in">
           <i className="fa fa-user-circle"></i>
-          <NavLink to='/login'>Sign In</NavLink>
-        </li>
-      </ul>
+          Sign In
+        </Link>
+      </div>
     </nav>
   );
 }
 
 export default Header;
+
