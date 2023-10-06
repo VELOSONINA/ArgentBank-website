f8cc3f981727eea5173387badeea485b2bc39a9e
Nirina VELOSONINA <vnirina081@gmail.com>
2023-10-06 11:39:23 +0200

    modification dans page home

--- argentbank/src/Pages/Home.js
+++ argentbank/src/Pages/Home.js
@@ -1,13 +1,21 @@
 import React from 'react';
+import Features from '../Components/Features'
 
-function Home() {
-  
+const Home = () => {
   return (
-    <div>
-      
-    </div>
+    <main>
+      <div className="hero">
+        <section className="hero-content">
+          <h2 className="sr-only">Promoted Content</h2>
+          <p className="subtitle">No fees.</p>
+          <p className="subtitle">No minimum deposit.</p>
+          <p className="subtitle">High interest rates.</p>
+          <p className="text">Open a savings account with Argent Bank today!</p>
+        </section>
+      </div>
+      <Features/>
+    </main>
   );
-}
-
+};
 
 export default Home;
