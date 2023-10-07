eb2b8bc286ea27ba3c7d2d1c57f574551deb8b5e
Nirina VELOSONINA <vnirina081@gmail.com>
2023-10-07 05:09:00 +0200

    modification de l'import index

--- argentbank/src/index.js
+++ argentbank/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
-import '../src/Styles/index.css';
+import './Styles/index.scss';
 import App from './App';
 
 //Redux
