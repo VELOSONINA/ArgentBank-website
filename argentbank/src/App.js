7b4b4dadf5c1fe44d1460dda9c2dba4734603b78
Nirina VELOSONINA <vnirina081@gmail.com>
2023-10-07 05:41:50 +0200

    ajout de App.scss

--- argentbank/src/App.js
+++ argentbank/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Header from './Components/Header';
 import RoutesTree from './Components/RoutesTree';
-import './App.css';
+import './App.scss';
 import Footer from './Components/Footer';
 
 function App() {
+  
   return (
     <div>
       <Header />
