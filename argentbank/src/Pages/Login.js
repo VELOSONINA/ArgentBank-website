c4bb4fff8208c725f05f5c3ab48cbadd2c8f822d
Nirina VELOSONINA <vnirina081@gmail.com>
2023-10-10 16:42:39 +0200

    modification du login

--- argentbank/src/Pages/Login.js
+++ argentbank/src/Pages/Login.js
@@ -1,56 +1,17 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
+import LoginForm from '../Components/login/loginForm';
 
-function Login() {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [rememberMe, setRememberMe] = useState(false);
-
-  useEffect(() => {
-
-  }, [username, password, rememberMe])
+const Login = () => {
 
   return (
     <main className="main bg-dark">
       <section className="sign-in-content">
         <i className="fa fa-user-circle sign-in-icon"></i>
-        <div className="login-page">
-          <h1>Sign In</h1>
-          <form>
-            <div className="input-wrapper">
-              <label htmlFor="username">Username</label>
-              <input
-                type="text"
-                id="username"
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
-              />
-            </div>
-            <div className="input-wrapper">
-              <label htmlFor="password">Password</label>
-              <input
-                type="password"
-                id="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-              />
-            </div>
-            <div className="input-remember">
-              <input
-                type="checkbox"
-                id="remember-me"
-                checked={rememberMe}
-                onChange={(e) => setRememberMe(e.target.checked)}
-              />
-              <label htmlFor="remember-me">Remember me</label>
-            </div>
-            <button className="sign-in-button" type="submit">
-              Sign In
-            </button>
-          </form>
-        </div>
+        <h1>Sign In</h1>
+        <LoginForm/>  
       </section>
     </main>
   );
-}
+};
 
 export default Login;
