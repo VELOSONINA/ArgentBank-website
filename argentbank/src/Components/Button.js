92311f8dbf491f47a907cb6b472262de78fdb1a2
Nirina VELOSONINA <vnirina081@gmail.com>
2023-10-10 16:52:12 +0200

    ajout du composant Button

---
+++ argentbank/src/Components/Button.js
@@ -0,0 +1,8 @@
+function Button({ children, className, onClick }) {
+    return (
+      <button className={className} onClick={onClick}>
+        {children}
+      </button>
+    );
+  }
+  export default Button;
