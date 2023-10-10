eed52040796ea4fd906a4d857ccc3e574a06de2c
Nirina VELOSONINA <vnirina081@gmail.com>
2023-10-10 16:36:38 +0200

    ajout du composants data

---
+++ argentbank/src/data/featuresData.js
@@ -0,0 +1,26 @@
+const featuresData = [
+    {
+      id: "1",
+      icon: '/images/icon-chat.png',
+      title: 'You are our #1 priority',
+      description:
+        'Need to talk to a representative? You can get in touch through our 24/7 chat or through a phone call in less than 5 minutes.',
+    },
+    {
+      id: "2",
+      icon: '/images/icon-money.png',
+      title: 'More savings means higher rates',
+      description:
+        'The more you save with us, the higher your interest rate will be!',
+    },
+    {
+      id: "3",
+      icon: '/images/icon-security.png',
+      title: 'Security you can trust',
+      description:
+        'We use top of the line encryption to make sure your data and money is always safe.',
+    },
+  ];
+  
+  export default featuresData;
+  
