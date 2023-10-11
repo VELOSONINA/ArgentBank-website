c267753bd2506c29b9e4505a1087ac7a0f153f75
Nirina VELOSONINA <vnirina081@gmail.com>
2023-10-11 04:00:14 +0200

    modification de accountsData

--- argentbank/src/Components/Users/Accounts.js
+++ argentbank/src/Components/Users/Accounts.js
@@ -1,20 +1,19 @@
 import React from "react";
-import { accountsData } from "../../data/accountsData";
 import Button from "../Button";
-
+import accountsData from "../../data/accountsData"; 
 
 function Accounts() {
   return (
     <>
       <h2 className="sr-only">Accounts</h2>
-      {accountsData.map((accounts) => {
+      {accountsData.map((account) => {
         return (
-          <section className="account" key={accounts.id}>
+          <section className="account" key={account.id}>
             <div className="account-content-wrapper">
-              <h3 className="account-title">{accounts.title}</h3>
-              <p className="account-amount">{accounts.amount}</p>
+              <h3 className="account-title">{account.title}</h3>
+              <p className="account-amount">{account.amount}</p>
               <p className="account-amount-description">
-                {accounts.description}
+                {account.description}
               </p>
             </div>
             <div className="account-content-wrapper cta">
