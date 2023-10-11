a6bf2a5dcdffba9deaa191a0f7b12d4ebf3d7601
Nirina VELOSONINA <vnirina081@gmail.com>
2023-10-11 04:09:12 +0200

    ajout des actions

---
+++ argentbank/src/actions/userAction.js
@@ -0,0 +1,21 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import axios from "axios";
+import { setUserName } from "../reducers/userSlice";
+
+export const editUserNameAsync = createAsyncThunk('user/editUserName', async (userName, { dispatch }) => {
+  try {
+    const response = await axios.put('http://localhost:3001/api/v1/user/profile', {
+      userName: userName,
+    });
+    
+    if (response.status === 200) {
+      const editData = response.data.body.userName;
+      dispatch(setUserName(editData));
+      return true;
+    }
+
+  } catch (error) {
+    console.error(`Error:`, error.response ? error.response.data.message : error.message);
+    return false;
+  }
+});
