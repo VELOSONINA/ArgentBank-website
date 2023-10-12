13105ef68474267bc7ac8a2c39e070385d1056c0
Nirina VELOSONINA <vnirina081@gmail.com>
2023-10-12 13:37:30 +0200

    modification dans reducers userslice

--- argentbank/src/reducers/userSlice.js
+++ argentbank/src/reducers/userSlice.js
@@ -1,37 +1,43 @@
 import { createSlice } from '@reduxjs/toolkit';
-import { editUserNameAsync } from '../actions/userAction';
+import { editUserNameAsync } from '../actions/userAction.js';
 
 const userSlice = createSlice({
   name: 'user',
   initialState: {
     userName: '',
     error: null, 
-    loading: false
+    loading: false,
+    isLoggedIn: false,
   },
   reducers: {
     setUserName: (state, action) => {
       state.userName = action.payload;
     },
+    logOut: (state) => {
+      state.userName = '';
+      state.isLoggedIn = false;
+    },
   },
   extraReducers: (builder) => {
     builder
-        .addCase(editUserNameAsync.pending, (state) => {
-            state.loading = true;
-            state.error = null; 
-        })
-        .addCase(editUserNameAsync.fulfilled, (state, action) => {
-            state.userName = action.payload;
-            state.loading = false; 
-            state.error = null;
-
-        })
-        .addCase(editUserNameAsync.rejected, (state, action) => {
-            state.error = action.error.message; // Enregistrez l'erreur dans votre état pour la gestion d'erreur.
-            state.loading = false;
+      .addCase(editUserNameAsync.pending, (state) => {
+        state.loading = true;
+        state.error = null; 
+      })
+      .addCase(editUserNameAsync.fulfilled, (state, action) => {
+        state.userName = action.payload;
+        state.loading = false; 
+        state.error = null;
+        state.isLoggedIn = true; 
+      })
+      .addCase(editUserNameAsync.rejected, (state, action) => {
+        state.error = action.error.message; 
+        state.loading = false;
+        state.isLoggedIn = false;
       });
   },
 });
 
-export const { setUserName } = userSlice.actions;
+export const { logOut } = userSlice.actions;
 
 export default userSlice.reducer;
