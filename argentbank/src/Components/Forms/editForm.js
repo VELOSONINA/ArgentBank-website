7ee31d5ebe501ae17f079813684398193722f244
Nirina VELOSONINA <vnirina081@gmail.com>
2023-10-12 13:30:31 +0200

    modification des formulaires

--- argentbank/src/Components/Forms/editForm.js
+++ argentbank/src/Components/Forms/editForm.js
@@ -1,86 +1,125 @@
-import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { editUserNameAsync } from '../../actions/userAction';
-import { setUserName } from '../../reducers/userSlice';
+import axios from "axios";
+import { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { selectUserData } from "../../reducers/selectors.js";
+import { toggleEdit } from "../../reducers/editUserSlice.js";
+import { editUserName } from "../../reducers/userDataSlice.js";
+import Button from "../Button";
 
-function EditForm() {
-  const [username, setUsername] = useState('');
-  const [firstName, setFirstName] = useState('');
-  const [lastName, setLastName] = useState('');
+export function EditForm() {
+  const [userName, setUserName] = useState("");
+  const [editSuccess, setEditSuccess] = useState(false);
+  const [editError, setEditError] = useState(false);
+
   const dispatch = useDispatch();
+  const userData = useSelector(selectUserData);
+  const firstName = userData.firstName;
+  const lastName = userData.lastName;
 
-  const handleUsernameChange = (e) => {
-    setUsername(e.target.value);
-  };
+  const token = localStorage.getItem("token");
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    dispatch(editUserNameAsync(username))
-      .then((result) => {
-        if (result) {
-          console.log('Mise à jour réussie.');
-          dispatch(setUserName(username));
-          setUsername('');
-        } else {
-          console.error('Échec lors de la mise à jour.');
+  const editUsername = async () => {
+    try {
+      const response = await axios.put(
+        "http://localhost:3001/api/v1/user/profile",
+        {
+          userName: userName,
+        },
+        {
+          headers: {
+            accept: "application/json",
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
         }
-      })
-      .catch((error) => {
-        console.error('Erreur lors de la mise à jour :', error);
-      });
+      );
+      const editData = await response.data;
+      dispatch(editUserName(editData.body.userName));
+
+      if (response.status === 200) {
+        setEditSuccess(true);
+      }
+    } catch (error) {
+      if (error.response) {
+        console.error("Server responded with status:", error.response.status);
+        console.error("Error message:", error.response.data.message);
+      } else if (error.request) {
+        console.error("No response received from the server.");
+      } else {
+        console.error("Error:", error.message);
+      }
+    }
   };
 
-  const handleClick = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    // Réinitialisation des champs
-    setUsername('username');
-    setFirstName('');
-    setLastName('');
-  }
+    const check = /^[0-9A-Za-z\s-]+$/;
 
+    setEditSuccess(false);
+    if (check.test(userName)) {
+      editUsername();
+      setUserName("");
+    } else {
+      setEditError(true);
+    }
+  };
+  const handleClick = async (e) => {
+    e.preventDefault();
+    dispatch(toggleEdit());
+    setUserName("");
+  };
+
+  const editSuccessMessage = () => {
+    if (editSuccess) {
+      return (
+        <p className="success-message">
+          Modification du nom d'utilisateur réussie!
+        </p>
+      );
+    }
+  };
+  const editErrorMessage = () => {
+    if (!editSuccess) {
+      return (
+        <p className="error-message">
+          Votre nom d'utilisateur ne peut pas contenir de caractères spéciaux,
+          veuillez rééssayer
+        </p>
+      );
+    }
+  };
   return (
-    <form id="edit-form" onSubmit={handleSubmit}>
-      <div className="input-wrapper">
-        <label htmlFor="username">Username:</label>
-        <input
-          type="text"
-          id="username"
-          name="username"
-          value={username}
-          onChange={handleUsernameChange}
-        />
-      </div>
-      <div className="input-wrapper">
-        <label htmlFor="firstName">First name:</label>
-        <input
-          type="text"
-          id="firstName"
-          name="firstName"
-          value={firstName}
-          disabled
-        />
-      </div>
-      <div className="input-wrapper">
-        <label htmlFor="lastName">Last name:</label>
-        <input
-          type="text"
-          id="lastName"
-          name="lastName"
-          value={lastName}
-          disabled
-        />
-      </div>
-      <div>
-        <button type="submit" className="edit-button">
-          Save
-        </button>
-        <button className="edit-button" onClick={handleClick}>
-          Cancel
-        </button>
-      </div>
-    </form>
+    <div className="editform--container">
+      {editSuccess === true ? editSuccessMessage() : null}
+      {editError === true ? editErrorMessage() : null}
+      <form type="submit" onSubmit={handleSubmit} className="form">
+        <div className="editform--input-wrapper">
+          <label htmlFor="username">Username:</label>
+          <input
+            type="text"
+            id="username"
+            value={userName}
+            onChange={(e) => setUserName(e.target.value)}
+            required
+          />
+        </div>
+        <div className="editform--input-wrapper">
+          <label htmlFor="firstName">First name:</label>
+          <input type="text" id="firstName" value={firstName} disabled />
+        </div>
+        <div className="editform--input-wrapper">
+          <label htmlFor="lastName">Last name:</label>
+          <input type="text" id="lastName" value={lastName} disabled />
+        </div>
+        <div className="button-wrapper">
+          <Button className="edit-button" type="submit">
+            Save
+          </Button>
+          <Button className="edit-button" onClick={handleClick}>
+            Cancel
+          </Button>
+        </div>
+      </form>
+    </div>
   );
 }
-
-export default EditForm;
