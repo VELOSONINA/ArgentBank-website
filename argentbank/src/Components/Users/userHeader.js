1cc129843aaf312bcd895ebae1755cfa3df249e4
Nirina VELOSONINA <vnirina081@gmail.com>
2023-10-12 13:31:46 +0200

    modification dans header user

--- argentbank/src/Components/Users/userHeader.js
+++ argentbank/src/Components/Users/userHeader.js
@@ -1,19 +1,48 @@
-import React from 'react'
-import Button from '../Button';
+import Button from "../Button";
+import { useSelector, useDispatch } from "react-redux";
+import { selectUserData, selectEditUser } from "../../reducers/selectors.js";
+import { toggleEdit } from "../../reducers/editUserSlice.js";
+import { EditForm } from "../Forms/editForm";
+import { userProfileInfo } from "../../api/userApi";
+import { useEffect } from "react";
+import { saveUserData } from "../../reducers/userDataSlice.js";
 
-function UserHeader({ userName }) {
+function UserHeader() {
+  const userData = useSelector(selectUserData);
+  const editData = useSelector(selectEditUser);
+  const dispatch = useDispatch();
+  const isLogging = localStorage.getItem("token");
+
+  useEffect(() => {
+    if (isLogging) {
+      const getData = async () => {
+        userProfileInfo(isLogging).then((response) =>
+          dispatch(saveUserData(response.data.body))
+        );
+      };
+      getData();
+    }
+  }, [dispatch, isLogging]);
+
   return (
     <div className="header">
+      {editData.open === true ? (
+        <h1>Edit user info</h1>
+      ) : (
         <h1>
           Welcome back
           <br />
-          {userName}
+          {`${userData.firstName} ${userData.lastName}`}
         </h1>
-        <Button className="edit-button" >
+      )}
+      {editData.open === true ? null : (
+        <Button className="edit-button" onClick={() => dispatch(toggleEdit())}>
           Edit name
         </Button>
+      )}
+
+      {editData.open === true ? <EditForm /> : null}
     </div>
   );
 }
-
-export default UserHeader
+export default UserHeader;
