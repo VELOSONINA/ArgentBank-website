fce29a24675fe823ecc5bcc0a58877118244c33e
Nirina VELOSONINA <vnirina081@gmail.com>
2023-10-12 13:34:00 +0200

    ajout de fonction dans l'appel api

--- argentbank/src/api/userApi.js
+++ argentbank/src/api/userApi.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const urlUserProfile = 'http://localhost:3001/api/user/profile';
 
-export const userProfile = async (isLogging) => {
+export const userProfileInfo = async (isLogging) => {
   try {
     const response = await axios.post(
       urlUserProfile,
