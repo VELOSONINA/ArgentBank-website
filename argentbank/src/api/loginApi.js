ea3720cae4be466f808aa23cc693e2197fd99396
Nirina VELOSONINA <vnirina081@gmail.com>
2023-10-14 04:35:50 +0200

    modif dans loginApi

--- argentbank/src/api/loginApi.js
+++ argentbank/src/api/loginApi.js
@@ -1,26 +1,58 @@
 import axios from 'axios';
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import { displayErrorMessage, clearErrorMessage } from "../reducers/errorMessageSlice";
+
+export const isUserLoggedIn = () => {
+  return localStorage.getItem("IsLoggedIn") === "true";
+};
 
 const urlUserLogin = 'http://localhost:3001/api/v1/user/login';
 
-export const loginUser = async ({ username, password }) => {
-    try {
-      
-      const response = await axios.post(urlUserLogin, {
-        username,
-        password,
-      });
+export const loginUser = createAsyncThunk(
+  "user/loginUser",
+  
+  async (credentials, { handleError, dispatch }) => {
+    try {  
+      const response = await axios.post(urlUserLogin, credentials);
 
-      if (response.status !== 200) {
-        throw new Error('Échec de la connexion.');
-      }
+      const token = response.data.body.token;
+      const responseData = response.data.data;
 
-      const data = response.data;
+      // Stocker le token dans le local storage
+      localStorage.setItem("IsLoggedIn", "true");
+      localStorage.setItem('token', token);
 
-      //stocker le token dans localstorage
-      localStorage.setItem('token', data.token);
-
-      return data.token; 
+      return responseData;
     } catch (error) {
-      throw new Error(`Erreur lors de la connexion:${error.message}`);
+      if (error.response) {
+        if (error.response.status === 400) { 
+          // Affiche le message d'erreur
+          dispatch(displayErrorMessage("Identifiants incorrects. Veuillez réessayer."));
+
+          // Efface le message d'erreur après 4 secondes
+          setTimeout(() => {
+            dispatch(clearErrorMessage());
+          }, 4000);
+
+          return handleError("Erreur d'authentification");
+        } else {
+          dispatch(displayErrorMessage("Une erreur est survenue. Veuillez réessayer."));
+
+          setTimeout(() => {
+            dispatch(clearErrorMessage());
+          }, 4000);
+
+          return handleError("Une erreur est survenue. Veuillez réessayer.");
+        }
+      } else {
+        dispatch(displayErrorMessage("Problème de connexion. Vérifiez votre réseau."));
+
+        setTimeout(() => {
+          dispatch(clearErrorMessage());
+        }, 4000);
+
+        return handleError("Problème de connexion. Vérifiez votre réseau.");
+      }
+    }
   }
-}
+);
