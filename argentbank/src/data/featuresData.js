07a208d5062a7924a47b35b0af2a256a0b1350d6
Nirina VELOSONINA <vnirina081@gmail.com>
2023-10-14 04:29:53 +0200

    modification dans data features

--- argentbank/src/data/featuresData.js
+++ argentbank/src/data/featuresData.js
@@ -1,21 +1,21 @@
 const featuresData = [
     {
       id: "1",
-      icon: '/images/icon-chat.png',
+      icon: '../../assets/images/icon-chat.png',
       title: 'You are our #1 priority',
       description:
         'Need to talk to a representative? You can get in touch through our 24/7 chat or through a phone call in less than 5 minutes.',
     },
     {
       id: "2",
-      icon: '/images/icon-money.png',
+      icon: '../../assets/images/icon-money.png',
       title: 'More savings means higher rates',
       description:
         'The more you save with us, the higher your interest rate will be!',
     },
     {
       id: "3",
-      icon: '/images/icon-security.png',
+      icon: '../../assets/images/icon-security.png',
       title: 'Security you can trust',
       description:
         'We use top of the line encryption to make sure your data and money is always safe.',
