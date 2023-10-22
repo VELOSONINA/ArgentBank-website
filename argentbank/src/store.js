93469394d939ad90f626c02cd8da182753586c2a
Nirina VELOSONINA <vnirina081@gmail.com>
2023-10-22 21:46:32 +0200

    modification du store

--- argentbank/src/store.js
+++ argentbank/src/store.js
@@ -1,16 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from './reducers/userSlice'; 
-import {userDataReducer} from './reducers/userDataSlice';
-import {editUserReducer} from "./reducers/editUserSlice";
-import errorMessageReducer from "./reducers/errorMessageSlice";
-
 
 const store = configureStore({
   reducer: {
-    user: userReducer,
-    userData: userDataReducer,
-    editUser: editUserReducer,
-    errorMessage: errorMessageReducer,
+    user: userReducer
   },
   devTools: true,
 });
