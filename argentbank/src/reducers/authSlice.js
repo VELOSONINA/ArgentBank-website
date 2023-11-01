7120f232c7ee23ac801a455232b76b5f2329847b
Nirina VELOSONINA <vnirina081@gmail.com>
2023-11-02 00:54:45 +0100

    renommer le userSlice en authSlice et modification des actions

--- argentbank/src/reducers/authSlice.js
+++ argentbank/src/reducers/authSlice.js
@@ -2,35 +2,38 @@ import { createSlice } from "@reduxjs/toolkit";
 import { authenticateUser , checkUserAuthentication } from '../Api/Authentication';
 
 
-const userSlice = createSlice({
-  name: "user",
+const authSlice = createSlice({
+  name: "auth",
   initialState: {
     loading: false,
+    user: null,
     error: null,
     isLoggedIn: checkUserAuthentication(),
   },
   reducers: {
     logOut: (state) => {
+      localStorage.removeItem("token");
+      state.user = null;
       state.isLoggedIn = false;
     },
   },
   extraReducers: (builder) => {
-    builder
-      .addCase(authenticateUser.pending, (state) => {
+    builder.addCase(authenticateUser.pending, (state) => {
         state.loading = true;
+        state.user = null;
         state.error = null;
         state.isLoggedIn = false;
       })
-      .addCase(authenticateUser.fulfilled, (state) => {
+      .addCase(authenticateUser.fulfilled, (state, action) => {
         state.loading = false;
+        state.user = action.payload;
         state.error = null;
         state.isLoggedIn = true;
       })
-      .addCase(authenticateUser.rejected, (state) => {
-        state.loading = false;
-        state.isLoggedIn = false;
+      .addCase(authenticateUser.rejected, (state, action) => {
+        state.error = action.payload;
       });
   },
 });
-export default userSlice.reducer;
-export const { logOut } = userSlice.actions;
+export default authSlice.reducer;
+export const { logOut } = authSlice.actions;
