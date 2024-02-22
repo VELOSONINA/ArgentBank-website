313631de298800854b33f34c8d0d978918405360
Nirina VELOSONINA <vnirina081@gmail.com>
2024-02-22 21:55:28 +0100

    mis en place de redux persist dans store.js

--- argentbank/src/store.js
+++ argentbank/src/store.js
@@ -1,11 +1,25 @@
 import { configureStore } from '@reduxjs/toolkit';
-import userReducer from './Reducers/authSlice'; 
+import {combineReducers} from "redux";
+import storage from 'redux-persist/lib/storage/session';
+import { persistReducer} from 'redux-persist';
+import thunk from 'redux-thunk';
+import authSliceReducer from './Reducers/authSlice'; 
+
+const reducers = combineReducers({
+  user: authSliceReducer
+})
+
+const persistConfig = {
+  key:'root' ,
+  storage ,
+  whitelist: ['user']
+}
+const persistedReducer = persistReducer ( persistConfig , reducers)
 
 const store = configureStore({
-  reducer: {
-    user: userReducer
-  },
-  devTools: true,
-});
+  reducer: persistedReducer,
+    devTools: process.env.NODE_ENV !== 'production',
+    middleware: [thunk]
+  })
 
 export default store;
