5ec3276d845fe92d0fcebd0e41f19d824dc17d82
Nirina VELOSONINA <vnirina081@gmail.com>
2024-03-06 22:44:28 +0100

    mise en place du toogleEditing et nom et prénom du user

--- argentbank/src/Pages/User.js
+++ argentbank/src/Pages/User.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import Accounts from "../Components/Utils/Accounts/Accounts";
 import EditUserInfo from "../Components/Utils/Forms/EditUserInfo";
 import Main from "../Components/Main";
@@ -9,59 +9,41 @@ import { Navigate } from "react-router-dom";
 
 function User() {
   const user = useSelector((state) => state.user);
-  const [isEditingName, setIsEditingName] = useState(false); 
-  const [isEditingInfo, setIsEditingInfo] = useState(false); 
+  const [isEditing, setIsEditing] = useState(false);
+ 
 
-  // Vérifier si l'utilisateur est actuellement connecté
   const isUserCurrentlyLoggedIn = user && user.isLoggedIn;
-
-  useEffect(() => {
-    // Mis à jour de l'état en fonction de la connexion de l'utilisateur
-    if (isUserCurrentlyLoggedIn) {
-      setIsEditingName(true);
-      setIsEditingInfo(true);
-    }
-
-    console.log("isEditingName:", isEditingName);
-    console.log("isEditingInfo:", isEditingInfo);
-  }, [isUserCurrentlyLoggedIn,isEditingName, isEditingInfo]);
-
+ 
   if (!isUserCurrentlyLoggedIn) {
-    return <Navigate to="/sign-in" />;
+     return <Navigate to="/sign-in" />;
   }
-  
-  const handleEditNameClick = () => {
-    setIsEditingName(false);
-    setIsEditingInfo(false);
-  };
 
-  const handleEditCancel = () => {
-    setIsEditingName(false);
-    setIsEditingInfo(false);
-  };
-
-  const handleSaveClick = () => {
-    setIsEditingInfo(true); 
-    setIsEditingName(true);
-  };
+  const toggleEditing = () => {
+    setIsEditing(current => !current);
+ };
 
   return (
-    <Main>
-      <GetTitle title="User page"/>
-      <div className="main bg-dark">
-      <div className="header">
-          {(!isEditingName && !isEditingInfo) && (
-            <>
-              <h1>Welcome back<br /> {`${user.firstName} ${user.lastName}`}!</h1>
-              <Button className="edit-button" onClick={handleEditNameClick}>Edit Name</Button>
-            </>
-          )}
-          {isEditingInfo && <EditUserInfo onCancel={handleEditCancel} onSave={handleSaveClick}/>}
-        </div>
-        <Accounts />
-      </div>
-    </Main>
+     <Main>
+       <GetTitle title="User page"/>
+       <div className="main bg-dark">
+         <div className="header">
+           {!isEditing && (
+             <>
+              <h1>Welcome back<br /> {`${user.user.firstName} ${user.user.lastName}`}!</h1>
+               <Button className="edit-button" onClick={toggleEditing}>Edit Name</Button>
+             </>
+           )}
+            {isEditing && (
+             <>
+               <EditUserInfo onCancel={toggleEditing} onSave={toggleEditing}/>
+             </>
+           )}
+         </div>
+         <Accounts />
+       </div>
+     </Main>
   );
-}
+ }
+ 
+ export default User;
 
-export default User;
