d730e2fc5ff112e871f5f9af7cc4597f4495fe79
Nirina VELOSONINA <vnirina081@gmail.com>
2024-12-23 22:21:19 +0100

    correction des indentations dans index.js

--- argentbank/src/index.js
+++ argentbank/src/index.js
@@ -3,8 +3,6 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './Styles/index.scss';
 import App from './App';
-
-//Redux
 import { Provider } from 'react-redux';
 import store from './store';
 
