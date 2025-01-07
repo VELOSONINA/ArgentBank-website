55e14c77e47dbfdb03a091710d5962c8320c9942
Nirina VELOSONINA <vnirina081@gmail.com>
2025-01-07 16:00:24 +0100

    refactorisation du composant Accounts

--- argentbank/src/Components/Utils/Accounts/Accounts.js
+++ argentbank/src/Components/Utils/Accounts/Accounts.js
@@ -1,27 +1,39 @@
 import React from "react";
+import PropTypes from "prop-types";
 import Button from "../../Button";
 import accountsData from "../../../Datas/accountsData"; 
 
+// Composant AccountItem
+function AccountItem({ id, title, amount, description }) {
+  return (
+    <section className="account" key={id}>
+      <div className="account-content-wrapper">
+        <h3 className="account-title">{title}</h3>
+        <p className="account-amount">{amount}</p>
+        <p className="account-amount-description">{description}</p>
+      </div>
+      <div className="account-content-wrapper cta">
+        <Button className="transaction-button">View transactions</Button>
+      </div>
+    </section>
+  );
+}
+
+AccountItem.propTypes = {
+  id: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  amount: PropTypes.number.isRequired,
+  description: PropTypes.string.isRequired,
+};
+
+// Composant Accounts
 function Accounts() {
   return (
     <>
       <h2 className="sr-only">Accounts</h2>
-      {accountsData.map((account) => {
-        return (
-          <section className="account" key={account.id}>
-            <div className="account-content-wrapper">
-              <h3 className="account-title">{account.title}</h3>
-              <p className="account-amount">{account.amount}</p>
-              <p className="account-amount-description">
-                {account.description}
-              </p>
-            </div>
-            <div className="account-content-wrapper cta">
-              <Button className="transaction-button">View transactions</Button>
-            </div>
-          </section>
-        );
-      })}
+      {accountsData.map((account) => (
+        <AccountItem key={account.id} {...account} />
+      ))}
     </>
   );
 }
